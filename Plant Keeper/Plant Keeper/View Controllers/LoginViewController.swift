69d1e423dacf8845eb445e54f8f3027722ed0217
Aria Rens <57843464+rens000@users.noreply.github.com>
2022-01-22 11:38:04 -0500

    add styling and fuctions to tap buttons

--- Plant Keeper/Plant Keeper/View Controllers/LoginViewController.swift
+++ Plant Keeper/Plant Keeper/View Controllers/LoginViewController.swift
@@ -8,14 +8,34 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpElements()
 
         // Do any additional setup after loading the view.
     }
     
+    func setUpElements() {
+        //hide error lavel
+        errorLabel.alpha = 0
+        
+        //style the elements
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleFilledButton(loginButton)
 
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    
     /*
     // MARK: - Navigation
 
--- Plant Keeper/Plant Keeper/View Controllers/SignUpViewController.swift
+++ Plant Keeper/Plant Keeper/View Controllers/SignUpViewController.swift
@@ -9,13 +9,38 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpElements()
 
         // Do any additional setup after loading the view.
     }
     
-
+    func setUpElements() {
+        //error label
+        errorLabel.alpha = 0
+        
+        //set up elements
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(signUpButton)
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
+    
     /*
     // MARK: - Navigation
 

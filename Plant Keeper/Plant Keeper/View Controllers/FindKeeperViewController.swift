d5f40c55c76960d8640b4341f78521fcb5890144
saumyaaverma <saumyaaverma@gmail.com>
2022-01-22 20:52:58 -0500

    stash

--- Plant Keeper/Plant Keeper/View Controllers/FindKeeperViewController.swift
+++ Plant Keeper/Plant Keeper/View Controllers/FindKeeperViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class FindKeeperViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let db = Firestore.firestore()
+        var ref = Firebase(url:MY_FIREBASE_URL)
+        ref.observeSingleEvent(of: .value) { snapshot in
+            for case let child as FIRDataSnapshot in snapshot.children {
+                guard let dict = child.value as? [String:Any] else {
+                    print("Error")
+                    return
+                }
+                let country = dict["country"] as Any
+                let city = dict["city"] as Any
+                print(longtitude)
+                print(latitude)
+            }
+        }
 
+        
+        
         // Do any additional setup after loading the view.
     }
+    
     
 
     /*

4c1f9ea8b5033d41dc337e69e1a5aea56c5dca30
Aria Rens <57843464+rens000@users.noreply.github.com>
2022-01-22 20:29:13 -0500

    Add label to BeKeeperViewController

--- Plant Keeper/Plant Keeper/View Controllers/BeKeeperViewController.swift
+++ Plant Keeper/Plant Keeper/View Controllers/BeKeeperViewController.swift
@@ -13,14 +13,11 @@ import FirebaseFirestore
 class BeKeeperViewController: UIViewController {
     
     @IBOutlet weak var countryTextField: UITextField!
-    
     @IBOutlet weak var cityTextField: UITextField!
-    
     @IBOutlet weak var postalCodeField: UITextField!
-    
     @IBOutlet weak var addressTextField: UITextField!
-    
     @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var addLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,36 +39,66 @@ class BeKeeperViewController: UIViewController {
         Utilities.styleTextField(postalCodeField)
         Utilities.styleTextField(addressTextField)
         Utilities.styleFilledButton(addButton)
+        addLabel.alpha = 0
 
     }
     @IBAction func addButtonTapped(_ sender: Any) {
         
-        let country = countryTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let city = cityTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let postalCode = postalCodeField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let address = addressTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let isAvailable = true
-        let db = Firestore.firestore()
-        let user = Auth.auth().currentUser
-        db.collection("users").whereField("uid", isEqualTo: user?.uid).getDocuments() { (querySnapshot, err) in
-                    let document = querySnapshot!.documents.first
-                    document?.reference.updateData([
-                        "country": country,
-                        "city": city,
-                        "postalCode": postalCode,
-                        "address": address,
-                        "isAvailable": isAvailable,
-                    ])
-            }
+        //validate fields
+        let error = validateFields()
+        
+        if error != nil {
+            //there is something wrong with the fields, show error message
+            //potential error
+            showError(message: error!)
+        }
+        
+        else {
+            let country = countryTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let city = cityTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let postalCode = postalCodeField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let address = addressTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let isAvailable = true
+            let db = Firestore.firestore()
+            let user = Auth.auth().currentUser
+            db.collection("users").whereField("uid", isEqualTo: user?.uid).getDocuments() { (querySnapshot, err) in
+                        let document = querySnapshot!.documents.first
+                        document?.reference.updateData([
+                            "country": country,
+                            "city": city,
+                            "postalCode": postalCode,
+                            "address": address,
+                            "isAvailable": isAvailable,
+                        ])
+                }
+            showDone()
+        }
+        
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func showError(message:String){
+        addLabel.text = message
+        addLabel.alpha = 1
     }
-    */
+    
+    func showDone() {
+        addLabel.text = "Thank you for signing up to be a plant keeper!  You will be contacted by potential matches in your area."
+        addLabel.alpha = 1
+    }
+    
+    //check the fields and vslidate that the data is correct
+    //if everything is correct return nil
+    //else return error message
+    func validateFields() -> String?{
+        //check all fields are filled in
+        if countryTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            cityTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            postalCodeField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            addressTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Please fill in all fields."
+        }
+        return nil
+    }
 
 }

b8f8bfbef56dbbcea07fd016800ba5f84c49ad6d
saumyaaverma <saumyaaverma@gmail.com>
2022-01-22 19:41:15 -0500

    update database with keeper info

--- Plant Keeper/Plant Keeper/View Controllers/KeeperViewController.swift
+++ Plant Keeper/Plant Keeper/View Controllers/KeeperViewController.swift
@@ -6,16 +6,64 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
 
 class KeeperViewController: UIViewController {
-
+    
+    @IBOutlet weak var countryTextField: UITextField!
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var postalCodeField: UITextField!
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpElements()
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "back")
+        backgroundImage.contentMode =  UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
 
         // Do any additional setup after loading the view.
     }
     
 
+    func setUpElements() {
+        
+        //style the elements
+        Utilities.styleTextField(countryTextField)
+        Utilities.styleTextField(cityTextField)
+        Utilities.styleTextField(postalCodeField)
+        Utilities.styleTextField(addressTextField)
+        Utilities.styleFilledButton(addButton)
+
+    }
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
+        let country = countryTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let city = cityTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let postalCode = postalCodeField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let address = addressTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let isAvailable = true
+        let db = Firestore.firestore()
+        let user = Auth.auth().currentUser
+        db.collection("users").whereField("uid", isEqualTo: user?.uid).getDocuments() { (querySnapshot, err) in
+                    let document = querySnapshot!.documents.first
+                    document?.reference.updateData([
+                        "country": country,
+                        "city": city,
+                        "postalCode": postalCode,
+                        "address": address,
+                        "isAvailable": isAvailable,
+                    ])
+            }
+    }
     /*
     // MARK: - Navigation
 
